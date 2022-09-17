a9c55232e88c78d4f078654d742c4e3db5617c2b
Daniel Velasquez <d.velasquezo@uniandes.edu.co>
2022-09-17 01:44:19 -0500

    Se agregan controladores vacios para socio y club y el interceptor de errores

--- src/socio/socio.module.ts
+++ src/socio/socio.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { SocioEntity } from './socio.entity';
 import { SocioService } from './socio.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { SocioController } from './socio.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([SocioEntity])],
-  providers: [SocioService]
+  providers: [SocioService],
+  controllers: [SocioController]
 })
 export class SocioModule {}
