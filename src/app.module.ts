c61de1a9e04d10ed480833e18214fc4159444e83
Daniel Velasquez <d.velasquezo@uniandes.edu.co>
2022-09-16 23:28:07 -0500

    Actualizando app module para compilacion de la app

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,23 @@ import { SocioModule } from './socio/socio.module';
 import { ClubModule } from './club/club.module';
 import { SocioClubModule } from './socio-club/socio-club.module';
 import { ClubSocioModule } from './club-socio/club-socio.module';
+import { ClubEntity } from './club/club.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { SocioEntity } from './socio/socio.entity';
 
 @Module({
-  imports: [SocioModule, ClubModule, SocioClubModule, ClubSocioModule],
+  imports: [SocioModule, ClubModule, SocioClubModule, ClubSocioModule, TypeOrmModule.forRoot({
+    type: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    username: 'postgres',
+    password: 'postgres',
+    database: 'clubs',
+    entities: [ClubEntity, SocioEntity],
+    dropSchema: true,
+    synchronize: true,
+    keepConnectionAlive: true
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
