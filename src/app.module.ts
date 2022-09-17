359209315209cfc152daf294b873a52915df49c1
Daniel Velasquez <d.velasquezo@uniandes.edu.co>
2022-09-16 21:46:19 -0500

    Se crean modulos socio-club club-socio

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SocioModule } from './socio/socio.module';
 import { ClubModule } from './club/club.module';
+import { SocioClubModule } from './socio-club/socio-club.module';
+import { ClubSocioModule } from './club-socio/club-socio.module';
 
 @Module({
-  imports: [SocioModule, ClubModule],
+  imports: [SocioModule, ClubModule, SocioClubModule, ClubSocioModule],
   controllers: [AppController],
   providers: [AppService],
 })
